07e1e488213e39f0593bbd7362e18ba4bbe21b1d
wayleung <610686949@qq.com>
2018-08-02 18:12:07 +0800

    api

--- datasource.js
+++ datasource.js
@@ -22,10 +22,14 @@ https://blog.csdn.net/qq_15987295/article/details/79489036
 defaultValue: 文件不存在时的默认值；
 serialize/deserialize: 写之前和读之后的操作。
 const adapter = new FilSync('db.json',{
-  serialize: (data) => encrypt(JSON.stringify(data)),
+  serialize: (data) => encrypt((data)),
   deserialize: (data) => JSON.parse(decrypt(data))
 
  */
+
+var require  = require || window.parent.require;
+var module =  module || window.parent.module;
+
 const _ = require('lodash');
 const lodashId = require('lodash-id');
 
@@ -36,9 +40,9 @@ const FileSync = require('lowdb/adapters/FileSync')
 
 /*const adapter = new FileSync('db.json')
 const db = low(adapter)*/
+var projectName = 'testPlanProject';
 
-
-var adapter = new FileSync('db.json')
+var adapter = new FileSync('./'+projectName+'/db.json')
 var db = low(adapter)
 db._.mixin(lodashId)
 _.mixin(lodashId)
@@ -57,8 +61,8 @@ class DataSource{
 
 
     //切换json文件(数据库) 返回的是整个json
-    changeJson(jsonfile) {
-        adapter = new FileSync(jsonfile)
+    setJson(jsonfile) {
+        adapter = new FileSync('./'+projectName+'/'+jsonfile)
         db = low(adapter)
         //console.log(lodashId)
         db._.mixin(lodashId)
@@ -67,11 +71,24 @@ class DataSource{
     }
 
     //返回当前json
-    getJson() {
+    getAllFromJson(){
         return db.value();
     }
 
 
+    getProjectName(){
+        return projectName;
+    }
+
+
+    setProjectName(inputProjectName){
+        projectName  = inputProjectName;
+    }
+
+
+    getAllFromJson(){
+        return (db.value());
+    }
 
 
 
@@ -196,8 +213,14 @@ class DataSource{
 
 
 
+
+
+
+
     //返回module基本信息
-    getModulenfo() {
+    getModuleInfo() {
+        this.setJson("modules.json");
+
         var size = db.value().length;
         //console.log(size)
         var obj = new Object();
@@ -207,49 +230,248 @@ class DataSource{
         for (var i = 0; i < size; i++) {
             var temp = db.get([i]).value();
 
-            var module = new Object();
-            module.moduleId = temp.moduleId;
-            module.text = temp.moduleName;
+            var mymodule = new Object();
+            mymodule.id    = temp.id;
+            mymodule.text = temp.moduleName;
             //console.log(temp.moduleId);
             //console.log(temp.moduleName);
-            modules[i] = module;
+            modules[i] = mymodule;
         }
         obj.children = modules;
         var result = new Array();
         result[0] = obj;
-        return JSON.stringify(result)
-
-
+        return result
     }
+
+
+
+    insertModule(newObject){
+        this.setJson("modules.json");
+
+        try{
+            db.insert(newObject).write();
+        }
+        catch (e) {
+            console.log(e);
+            return false
+        }
+        return true
+    }
+
+
+/*    removeModule(inputModuleName){
+        try{
+            db.remove({ moduleName: inputModuleName }).write()
+        }
+        catch (e) {
+            console.log(e);
+            return false
+        }
+        return true
+    }*/
+
+
+    removeModule(inputModuleId){
+        this.setJson("modules.json");
+
+        try{
+            db.remove({ id: inputModuleId }).write()
+        }
+        catch (e) {
+            console.log(e);
+            return false
+        }
+        return true
+    }
+
+
+    updateModule(newObject){
+        this.setJson("modules.json");
+
+        try{
+            var temp =  new Object();
+            temp.id = newObject.id;
+            db.find(temp).assign(newObject).write();
+        }
+        catch (e) {
+            console.log(e);
+            return false
+        }
+        return true
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     //返回Case基本信息
     getCaseInfo(moduleId){
+        this.setJson("modules.json");
+
         //console.log(db.get([0]).value().testCases);
         //console.log(db.value());
-        var module = _.getById(db.value(), moduleId);
-        //console.log(module);
-        var cases = module.testCases;
+        var mymodule = _.getById(db.value(), moduleId);
+        //console.log(mymodule);
+        var cases = mymodule.testCases;
         var casesArray  = new Array();
         for(var i=0;i<cases.length;i++){
             var temp = new Object();
             //console.log(cases[i].caseName);
             //console.log(cases[i].status);
             //console.log(cases[i].comment);
-            temp['Test Case Name']  = cases[i].caseName;
-            temp.Comment  = cases[i].comment;
-            temp.Execute =  cases[i].status;
+            temp.id  = cases[i].id;
+            temp.caseName  = cases[i].caseName;
+            temp.comment  = cases[i].comment;
+            temp.status =  cases[i].status;
             casesArray[i] = temp;
         }
-        return JSON.stringify(casesArray);
+        return casesArray;
+    }
+
+
+    insertCase(moduleId,object){
+        this.setJson("modules.json");
+
+       /* var temp = new Object();*/
+        //console.log(cases[i].caseName);
+        //console.log(cases[i].status);
+        //console.log(cases[i].comment);
+/*        temp.id  = object.id;
+        temp.caseName  = object['Test Case Name'];
+        temp.comment  = object.Comment;
+        temp.status =  object.Execute;
+        temp.scripts =  [];
+        temp.datas =  [];*/
+
+
+        var cases  = db.find({id:moduleId}).get('testCases');
+        //console.log(cases);
+      /* var mymodule = _.getById(db.value(), moduleId);
+
+       var cases =  mymodule.find("testCases");
+*/
+        var flag = false;
+        try{
+            cases.insert(object).write();
+        }catch (e) {
+            console.log(e)
+            return false;
+        }
+
+        return true;
     }
 
 
 
+    removeCase(caseId){
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+           /* var kase =  cases.find({ id: caseId }).value();
+
+            if(kase!=undefined&&kase!=null){
+                return kase;
+            }*/
+
+            cases.remove({ id: caseId }).write()
+        }
+
+
+       /* return (db.remove({ objectPageName: inputObjectPageName }).write());*/
+    }
+
+    removeCaseByArray(caseIdArray) {
+
+
+        for (var i = 0; i < caseIdArray.length; i++) {
+
+            this.removeCase(caseIdArray[i]);
+        }
+
+
+    }
+
+    updateCase(object){
+/*        var temp = new Object();*/
+        //console.log(cases[i].caseName);
+        //console.log(cases[i].status);
+        //console.log(cases[i].comment);
+/*        temp.id  = object.id;
+        temp.caseName  = object['Test Case Name'];
+        temp.comment  = object.Comment;
+        temp.status =  object.Execute;*/
+
+
+        this.setJson("modules.json");
+
+        var caseId =  object.id;
+
+
+        var modules  = db.value();
+
+
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            /* var kase =  cases.find({ id: caseId }).value();
+
+             if(kase!=undefined&&kase!=null){
+                 return kase;
+             }*/
+
+            cases.find({ id: caseId }).assign(object).write();
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
     //返回script信息
-    getScriptInfo(moduleId,caseId){
-        var module = _.getById(db.value(), moduleId);
-        var cases = module.testCases;
+   /* getScriptInfo(moduleId,caseId){
+        this.setJson("modules.json");
+
+
+        var mymodule = _.getById(db.value(), moduleId);
+        var cases = mymodule.testCases;
         var kase = _.getById(cases, caseId);
         console.log(kase);
         var scripts = kase.scripts;
@@ -265,14 +487,174 @@ class DataSource{
             temp.Comments  = scripts[i].comment;
             scriptsArray[i] = temp;
         }
-        return JSON.stringify(scriptsArray);
+        return (scriptsArray);
+    }*/
+
+    getScriptInfo(caseId){
+/*
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']').value();
+            var kase =  _.getById(mymodule.testCases,caseId);
+            if(kase!=undefined&&kase!=null){
+                return kase;
+            }
+        }
+*/
+
+
+        //var kase = _.getById(db.value(), caseId);
+
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase = temp;
+                break;
+            }
+        }
+
+        var scripts  = kase.get("scripts").value();
+
+        return scripts;
+
+
+
     }
 
 
+
+
+
+
+    insertScript(caseId,object){
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase =  temp;
+                break;
+            }
+        }
+
+        var scripts  = kase.get("scripts");
+
+        var flag = false;
+        try{
+            scripts.insert(object).write();
+        }catch (e) {
+            console.log(e)
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+
+
+
+    removeScript(scriptId){
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase =  temp;
+                break;
+            }
+        }
+
+        var scripts  = kase.get("scripts");
+
+        scripts.remove({ id: scriptId }).write()
+
+
+
+
+
+    }
+
+
+    updateScript(object){
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var scriptId  = object.id;
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase =  temp;
+                break;
+            }
+        }
+
+        var scripts  = kase.get("scripts");
+
+        scripts.find({ id: scriptId }).assign(object).write();
+    }
+
+
+
+
+
+
+
+
     //返回script data 信息
-    getScriptDataInfo(moduleId,caseId){
-        var module = _.getById(db.value(), moduleId);
-        var cases = module.testCases;
+    /*getScriptDataInfo(moduleId,caseId){
+        this.setJson("modules.json");
+
+        var mymodule = _.getById(db.value(), moduleId);
+        var cases = mymodule.testCases;
         var kase = _.getById(cases, caseId);
         console.log(kase);
         var datas = kase.datas;
@@ -285,11 +667,176 @@ class DataSource{
             temp['No.']  = datas[i].no;
             datasArray[i] = temp;
         }
-        return JSON.stringify(datasArray);
+        return (datasArray);
+    }*/
+
+
+
+/*    getScriptDataInfo(caseId){
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']').value();
+            var kase =  _.getById(mymodule.testCases,caseId);
+            if(kase!=undefined&&kase!=null){
+                return kase.datas;
+            }
+        }
+    }*/
+
+
+    getDataInfo(caseId){
+        /*
+                this.setJson("modules.json");
+
+                var modules  = db.value();
+                for(var i =0;i<modules.length;i++){
+                    var mymodule = db.get('['+i+']').value();
+                    var kase =  _.getById(mymodule.testCases,caseId);
+                    if(kase!=undefined&&kase!=null){
+                        return kase;
+                    }
+                }
+        */
+
+
+        //var kase = _.getById(db.value(), caseId);
+
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase = temp;
+                break;
+            }
+        }
+
+        var datas  = kase.get("datas").value();
+
+        return datas;
+
+
+
     }
 
 
-    
+
+
+
+
+    insertData(caseId,object){
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase =  temp;
+                break;
+            }
+        }
+
+        var datas  = kase.get("datas");
+
+        var flag = false;
+        try{
+            datas.insert(object).write();
+        }catch (e) {
+            console.log(e)
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+
+
+
+    removeData(datasId){
+
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase =  temp;
+                break;
+            }
+        }
+
+        var datas  = kase.get("datas");
+
+        datas.remove({ id: datasId }).write()
+
+
+
+
+
+    }
+
+
+    updateData(object){
+        this.setJson("modules.json");
+
+        var modules  = db.value();
+
+        var datasId  = object.id;
+
+        var kase;
+        for(var i =0;i<modules.length;i++){
+            var mymodule = db.get('['+i+']');
+
+            var cases = mymodule.get("testCases");
+
+            //console.log(cases.value());
+
+            var temp =  cases.find({ id: caseId });
+
+            if(temp!=undefined&&temp!=null){
+                kase =  temp;
+                break;
+            }
+        }
+
+        var datas  = kase.get("datas");
+
+        datas.find({ id: datasId }).assign(object).write();
+    }
 
 
 
@@ -300,7 +847,137 @@ class DataSource{
 
 
 
-            test() {
+
+
+
+
+
+
+
+
+
+
+    //pageinfo
+
+
+    getObjectPage(inputObjectPageName){
+
+        this.setJson("objectPages.json");
+
+        var temp =  new Object();
+        temp.objectPageName = inputObjectPageName;
+        console.log(temp)
+        return (db.find(temp).value());
+    }
+
+
+
+
+    insertObjectPage(object){
+        this.setJson("objectPages.json");
+
+		var flag = false;
+        try{
+            db.insert(object).write();
+        }catch (e) {
+            console.log(e)
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    removeObjectPage(inputObjectPageName){
+        this.setJson("objectPages.json");
+
+        return (db.remove({ objectPageName: inputObjectPageName }).write());
+    }
+
+
+    updateObjectPage(object){
+        this.setJson("objectPages.json");
+
+        var temp =  new Object();
+        temp.objectPageName = object.objectPageName;
+        return db.find(temp).assign(object).write();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //返回gloabaldata信息
+    getGlobalData(globaldataId){
+        this.setJson("globalDatas.json");
+
+        console.log(db.value());
+        var globaldata = _.getById(db.value(), globaldataId);
+        return (globaldata);
+    }
+
+
+    insertGlobalData(object){
+        this.setJson("globalDatas.json");
+
+
+        //console.log(db.get('[0]').value());
+        var flag = false;
+        try{
+            db.insert(object).write();
+        }catch (e) {
+            console.log(e)
+            return false;
+        }
+
+        return true;
+    }
+
+
+/*
+    //批量删除 按条件
+    removeGlobalData(){
+       return db.remove({ key: 'url22' })
+            .write()
+    }*/
+
+
+
+    removeGlobalData(gloabalDataId){
+
+        this.setJson("globalDatas.json");
+
+        return (db.remove({ id: gloabalDataId }).write());
+    }
+
+
+    updateGlobalData(object){
+        this.setJson("globalDatas.json");
+
+        var temp =  new Object();
+        temp.id = object.id;
+        return (db.find(temp).assign(object).write());
+    }
+
+
+
+
+
+
+
+    test() {
         //db=changeJson('db.json');
         adapter = new FileSync('testPlanProject.json')
         db = low(adapter)
@@ -320,3 +997,20 @@ class DataSource{
 
 //export default DataSource
 module.exports = DataSource;
+
+/*module.exports = {
+    DataSource : DataSource,
+    getDataSource: function(inputProjectName) {
+        projectName  = inputProjectName;
+        return DataSource;
+    }
+}*/
+
+// module.exports.getDataSource  function(inputProjectName,inputJsonfile) {
+//       adapter = new FileSync('./'+inputProjectName+'/'+inputJsonfile)
+//         db = low(adapter)
+//         //console.log(lodashId)
+//         db._.mixin(lodashId)
+//         _.mixin(lodashId)
+// 		return DataSource;
+// };
